eece8e8b679b4f58aefa6ed769b5fdbed68ee448
John Cardinal <jfcardinal@gmail.com>
2020-12-09 16:47:40 -0500

    Add four new tables - Swedish regional archive location from code - Swedish regional archive name from code - US State+City to County - US State+County to County

---
+++ se_region_location.js
@@ -0,0 +1,15 @@
+﻿const se_region_location = {
+
+  "GLA": "Göteborg",
+  "HLA": "Härnösand",
+  "KRA": "Stockholm",
+  "LLA": "Lund",
+  "MSA": "Malmö",
+  "OLA": "Östersund",
+  "SSA": "Stockholm",
+  "ULA": "Uppsala",
+  "VA": "Karlstad",
+  "VALA": "Vadstena",
+  "VILA": "Visby",
+
+};
